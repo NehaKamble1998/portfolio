368840b046ad8522a9bd4e82084bc72c69bfb312
Neha Kamble <knehap97@gmail.com>
2025-04-13 23:34:21 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./App.module.css";
+import NavBar from "./Components/NavBar/NavBar";
+
+const App = () => {
+  return (
+    <div className={styles.App}>
+      <NavBar />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import styles from "./NavBar.module.css";
+import { HiMenuAlt2 } from "react-icons/hi";
+import { MdClose } from "react-icons/md";
+const NavBar = () => {
+  const [menu, setMenu] = useState("closed");
+  return (
+    <nav className={styles.navbar}>
+      <a href="/" className={styles.title}>
+        Portfolio
+      </a>
+      <div className={styles.menu}>
+        {menu === "closed" && (
+          <HiMenuAlt2
+            className={styles.burger_menu}
+            style={{ height: "3em", width: "3em" }}
+            onClick={() => setMenu("opened")}
+          />
+        )}
+        {menu === "opened" && (
+          <MdClose
+            className={styles.burger_menu}
+            style={{ height: "3em", width: "3em" }}
+            onClick={() => setMenu("closed")}
+          />
+        )}
+
+        <ul
+          className={`${styles.menuItems} ${
+            menu === "opened" && styles.menu_opened
+          }`}
+          onClick={() => setMenu("closed")}
+        >
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
